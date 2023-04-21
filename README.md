7b81218acdebf84b0fb307edea76c8829ae71ec4
heysaeid <heysaeid92@gmail.com>
2023-04-21 20:25:26 +0330

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# FastAPI Fast Template
+This library helps you not get involved in the complexities of FastAPI and its libraries and focus all your attention on the application.
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name="FastAPI Fast Template",
+    install_requires=[
+        "fastapi==0.95.1",
+        "uvicorn==0.21.1",
+        "pydantic==1.10.7",
+        "python-dotenv==1.0.0",
+    ]
+)
