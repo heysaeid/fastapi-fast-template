13a7c646398ca5fcaf7e779f7721fc6878af71bf
heysaeid <heysaeid92@gmail.com>
2023-05-02 18:42:16 +0330

    Add ActionClass to cli

---
+++ src/cli.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Action:
+    
+    def create_dirs(directories: List[str]) -> None:
+        pass
+    
+    def create_files(files: List[str], check_exists_file: bool = True) -> None:
+        pass
+    
+    def init() -> None:
+        pass
