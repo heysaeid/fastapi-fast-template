a7aa170dcdf2ef6d8b29c221379268a8f70ea1f4
heysaeid <heysaeid92@gmail.com>
2023-05-03 23:29:16 +0330

    Add .gitignore file to create_files method

--- src/cli.py
+++ src/cli.py
@@ -1,5 +1,6 @@
 import os
 from typing import List
+from content import BaseContent
 
 
 class Action:
@@ -31,8 +32,39 @@ class Action:
         if created_dirs:
             print(f"The following directories were successfully created: {', '.join(created_dirs)}")
     
-    def create_files(files: List[str], check_exists_file: bool = True) -> None:
-        pass
+    def create_files(files: List[str], exist_ok: bool = True) -> None:
+        """
+        Create files if they don't exist.
+
+        Args:
+            files (List[str]): A list of file paths to create.
+            exist_ok (bool, optional): A flag indicating whether to check if the file already exists before creating it.
+                Defaults to True.
+
+        Returns:
+            None
+
+        Examples:
+            >>> create_files(['./src/config.py', './src/main.py', './.gitignore'])
+            The following files were successfully created: ./src/config.py, ./src/main.py, ./.gitignore
+
+        """
+        files_with_action = {
+            "./.gitignore": BaseContent.get_gitignore,
+        }
+        created_files = []
+        for file in files:
+            if not exist_ok or not os.path.exists(file):
+                with open(file, 'w') as f:
+                    file_action = files_with_action.get(file)
+                    if file_action:
+                        f.write(file_action())
+                created_files.append(file)
+            else:
+                print(f"{file} already exists")
+        if created_files:
+            print(f"The following files were successfully created: {', '.join(created_files)}")
+
     
     @classmethod
     def init(cls) -> None:
