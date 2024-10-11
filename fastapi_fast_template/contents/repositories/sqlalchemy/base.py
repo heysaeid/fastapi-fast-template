ee7b33a6d01892eefc049f925cc98f246b621385
heysaeid <heysaeid92@gmail.com>
2024-10-11 23:54:51 +0330

    🐛 Changing default commit values ​​in sqlalchemy.BaseRepository

--- fastapi_fast_template/contents/repositories/sqlalchemy/base.py
+++ fastapi_fast_template/contents/repositories/sqlalchemy/base.py
@@ -154,7 +154,7 @@ class BaseRepository(Generic[ModelType]):
     async def create(
         self,
         entity: ModelType = None,
-        commit: bool = False,
+        commit: bool = True,
         **kwargs,
     ) -> ModelType:
         """
@@ -193,7 +193,7 @@ class BaseRepository(Generic[ModelType]):
     async def update(
         self,
         entity: ModelType,
-        commit: bool = False,
+        commit: bool = True,
         **kwargs,
     ) -> ModelType:
         """
